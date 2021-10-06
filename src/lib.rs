13e33bab57c0fb441cbb642f888144c6c0ccb4df
Eugene Lomov <eugene.v.lomov@gmail.com>
2021-10-06 21:26:37 +0300

    Rewriten from_u8_slice in completely safe rust
    + added test-case crate for more maintainable tests

--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,4 @@
+#![feature(slice_as_chunks)]
 pub mod board;
 pub mod card;
 pub mod vm;
--- src/vm/memory/slice_u8_vec.rs
+++ src/vm/memory/slice_u8_vec.rs
@@ -2,18 +2,13 @@ use crate::word::Word;
 use std::{mem, slice};
 use std::ops::Index;
 use tinyvec::SliceVec;
+use std::convert::TryInto;
+
 
 #[derive(Eq, PartialEq, Debug)]
 pub struct InternalStack<'a>(SliceVec<'a, InternalWord>);
 
-#[derive(Eq, PartialEq, Debug)]
-struct InternalWord(u32, i32);
-
-impl Default for InternalWord {
-    fn default() -> Self {
-        InternalWord(0, 0)
-    }
-}
+type InternalWord = [u8; 5];
 
 impl<'a> InternalStack<'a> {
     pub fn from_u8_slice(slice: &'a mut [u8], new_len: usize) -> Self {
@@ -22,24 +17,12 @@ impl<'a> InternalStack<'a> {
         // Make sure we are going to read a full chunk of stuff
         assert_eq!(len % element_size, 0);
 
-        let (head, body, tail) = unsafe { slice.align_to::<InternalWord>() };
-        assert!(head.is_empty());
-        assert!(tail.is_empty());
+        // Remainer will always be empty, because we've already checked slice length
+        let (new_slice, _) = slice.as_chunks_mut();
 
-        let data = body.as_ptr();
-
-        unsafe {
-            // Don't allow the current slice to be dropped
-            // (which would invalidate the memory)
-            mem::forget(body);
-
-            let new_slice =
-                slice::from_raw_parts_mut(data as *mut InternalWord, len / element_size);
-
-            InternalStack(SliceVec::<'a, InternalWord>::from_slice_len(
+        InternalStack(SliceVec::<'a, InternalWord>::from_slice_len(
                 new_slice, new_len,
-            ))
-        }
+        ))
     }
 
     pub fn len(&self) -> usize {
@@ -48,20 +31,34 @@ impl<'a> InternalStack<'a> {
 
     pub fn pop(&mut self) -> Option<Word> {
         let internal_word = self.0.pop();
-        internal_word.map(|word| match word {
-            InternalWord(0, 0) => Word::Boolean(false),
-            InternalWord(0, 1) => Word::Boolean(true),
-            InternalWord(0, _) => panic!("Memory corrupted!"),
-            InternalWord(1, i) => Word::Numeric(i),
-            InternalWord(_, _) => panic!("Memory corrupted!"),
+        internal_word.map(|word| {
+            let descriminant = word[0];
+            match descriminant {
+                0 => {
+                    let bool_data = word[1];
+                    match bool_data {
+                        0 => Word::Boolean(false),
+                        1 => Word::Boolean(true),
+                        _ => panic!("Memory corrupted!"),
+                    }
+                }
+                1 => {
+                    Word::Numeric(i32::from_le_bytes(word[1..].try_into().unwrap()))
+                }
+                _ => panic!("Memory corrupted!"),
+            }
         })
     }
 
     pub fn push(&mut self, word: Word) {
         let internal_word = match word {
-            Word::Boolean(false) => InternalWord(0, 0),
-            Word::Boolean(true) => InternalWord(0, 1),
-            Word::Numeric(val) => InternalWord(1, val),
+            Word::Boolean(false) => [0,0,0,0,0],
+            Word::Boolean(true) => [0,1,0,0,0],
+            Word::Numeric(val) => {
+                let mut arr = [1,0,0,0,0];
+                arr[1..].clone_from_slice(&val.to_le_bytes());
+                arr
+            },
         };
         self.0.push(internal_word);
     }
@@ -72,23 +69,61 @@ mod internal_stack_tests {
     use super::*;
     use crate::word_vec;
     use tinyvec::array_vec;
+    use test_case::test_case;
+    use std::iter;
 
     #[test]
-    fn alligned() {
-        let mut raw_data = vec![0, 0, 0, 0, 0, 0, 0, 0];
+    fn bool_false() {
+        let mut raw_data = vec![0, 0, 0, 0, 0];
 
-        let mut stack = unsafe { InternalStack::from_u8_slice(&mut raw_data, 1) };
+        let mut stack = InternalStack::from_u8_slice(&mut raw_data, 1);
         assert_eq!(stack.pop(), Some(Word::Boolean(false)));
+        assert_eq!(stack.len(), 0);
+    }
+
+    #[test]
+    fn bool_true() {
+        let mut raw_data = vec![0, 1, 0, 0, 0];
+
+        let mut stack = InternalStack::from_u8_slice(&mut raw_data, 1);
+        assert_eq!(stack.pop(), Some(Word::Boolean(true)));
         assert_eq!(stack.len(), 0);
     }
 
     #[test]
     #[should_panic]
-    fn unalligned() {
-        let mut raw_data = vec![1, 0, 0, 0, 0, 0, 0, 0, 0];
+    fn bool_corrupted() {
+        let mut raw_data = vec![0, 2, 0, 0, 0];
 
-        let mut stack = unsafe { InternalStack::from_u8_slice(&mut raw_data[1..], 1) };
-        assert_eq!(stack.pop(), Some(Word::Boolean(false)));
+        let mut stack = InternalStack::from_u8_slice(&mut raw_data, 1);
+        assert_eq!(stack.pop(), Some(Word::Boolean(true)));
+    }
+
+    #[test_case(0)]
+    #[test_case(1)]
+    #[test_case(i32::MIN)]
+    #[test_case(i32::MAX)]
+    fn numeric(num: i32) {
+        let mut raw_data = vec![1];
+        raw_data.extend(num.to_le_bytes());
+
+        let mut stack = InternalStack::from_u8_slice(&mut raw_data, 1);
+        assert_eq!(stack.pop(), Some(Word::Numeric(num)));
         assert_eq!(stack.len(), 0);
     }
+
+    #[test_case(Word::Numeric(0))]
+    #[test_case(Word::Numeric(1))]
+    #[test_case(Word::Numeric(i32::MIN))]
+    #[test_case(Word::Numeric(i32::MAX))]
+    #[test_case(Word::Boolean(true))]
+    #[test_case(Word::Boolean(false))]
+    fn push_pop_equivalence(word: Word) {
+        let mut raw_data: Vec<u8> = iter::repeat(0).take(mem::size_of::<InternalWord>()).collect();
+        let mut stack = InternalStack::from_u8_slice(&mut raw_data, 0);
+        stack.push(word);
+        assert_eq!(stack.pop().unwrap(), word);
+        
+    }
+
 }
