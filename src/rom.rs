4a90b10fe9bdd0317d19ba2ece2608d2aac36eee
Eugene Lomov <eugene.v.lomov@gmail.com>
2021-11-26 23:49:04 +0300

    Created replacement structs for Rom

--- src/rom.rs
+++ src/rom.rs
@@ -10,6 +10,77 @@ pub struct Rom {
     instructions: Vec<VMCommand>,
     initial_board_state: Board,
 }
+
+#[derive(Debug, Eq, PartialEq)]
+pub struct InstructionRom<'a> {
+    instructions: &'a [VMCommand],
+}
+
+impl<'a> InstructionRom<'a> {
+    #[must_use]
+    pub fn fetch_instruction(&self, pc: usize) -> VMCommand {
+        self.instructions[pc]
+    }
+
+    #[must_use]
+    pub unsafe fn from_raw_parts(instructions: &'a [VMCommand]) -> Self {
+        Self { instructions }
+    }
+}
+
+#[derive(Debug, Eq, PartialEq)]
+pub struct CardTypesRom<'a> {
+    card_types: &'a [CardType],
+}
+
+impl<'a> CardTypesRom<'a> {
+    #[must_use]
+    pub fn card_type_count(&self) -> usize {
+        self.card_types.len()
+    }
+
+    #[must_use]
+    pub fn card_type_by_type_index(&self, type_index: usize) -> &CardType {
+        &self.card_types[type_index]
+    }
+
+    #[must_use]
+    pub fn card_type_by_type_id(&self, type_id: u32) -> Option<&CardType> {
+        self.card_types
+            .iter()
+            .find(|card_type| card_type.id() == type_id)
+    }
+
+    pub fn instance_card_by_type_id(&self, type_id: u32, card_id: u32) -> Result<Card, Error> {
+        let typ = &self.card_types.iter().find(|typ| typ.id() == type_id);
+        match typ {
+            Some(typ) => Ok(typ.instantiate_card(card_id)),
+            None => Err(Error::NoSuchTypeId(type_id)),
+        }
+    }
+
+    pub fn instance_card_by_type_index(
+        &self,
+        type_index: u32,
+        card_id: u32,
+    ) -> Result<Card, Error> {
+        let index: usize = type_index as usize;
+        if index < self.card_types.len() {
+            let typ: &CardType = &self.card_types[index];
+            Ok(typ.instantiate_card(card_id))
+        } else {
+            Err(Error::TypeIndexOutOfBounds {
+                index: type_index,
+                len: self.card_types.len() as u32,
+            })
+        }
+    }
+    #[must_use]
+    pub unsafe fn from_raw_parts(card_types: &'a [CardType]) -> Self {
+        Self { card_types }
+    }
+}
+
 impl Rom {
     #[must_use]
     pub fn fetch_instruction(&self, pc: usize) -> VMCommand {
