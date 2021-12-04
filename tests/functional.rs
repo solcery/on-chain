bd0191c1996c0dcbdbb2fa23c0ad6aa1bb909078
Eugene Lomov <eugene.v.lomov@gmail.com>
2021-12-04 14:40:54 +0300

    Removed code duplication in integration tests

--- tests/functional.rs
+++ tests/functional.rs
@@ -21,35 +21,10 @@ use solcery_vm::{
 
 #[tokio::test]
 async fn initialize_dummy() {
-    let program_id = Pubkey::new_unique();
-    let rom_id = Pubkey::new_unique();
-    let board_id = Pubkey::new_unique();
-
-    // This is the Pubkey of the solcery engine, which will be the only entity, who sends
-    // transactions to the Solcery VM
-    let engine_keypair = Keypair::new();
-    let engine_id = engine_keypair.try_pubkey().unwrap();
-
     let rom = generate_testing_rom();
-    let rom = rom.try_to_vec().unwrap();
-    let mut rom_account = AccountSharedData::new(1_000, 1024, &engine_id);
-    rom_account.set_data(rom);
-
     let board = unsafe { Board::from_raw_parts(vec![], word_vec![], 0) };
-    let mut board = board.try_to_vec().unwrap();
-    board.extend(std::iter::repeat(0).take(24 - board.len()));
-    // Actually board should be also owned by engine, however it is impossible to test it's
-    // behavior under solana-program-test (because engine_keypair should be the payer)
-    let mut board_account = AccountSharedData::new(1_000, 1024, &program_id);
-    board_account.set_data(board);
 
-    let mut program = ProgramTest::new("solcery_vm", program_id, processor!(process_instruction));
-    program.add_account(rom_id, Account::from(rom_account));
-    program.add_account(board_id, Account::from(board_account));
-
-    let (mut banks_client, payer, recent_blockhash) = program.start().await;
-
-    // Add dummy
+    // Add dummy command
     let mut instruction_bytes = vec![0];
     let cardtype_index_bytes = u32::to_le_bytes(0);
     let action_index_bytes = u32::to_le_bytes(0);
@@ -60,34 +35,37 @@ async fn initialize_dummy() {
     let arg_bytes = Vec::<Word>::new().try_to_vec().unwrap();
     instruction_bytes.extend_from_slice(&arg_bytes);
 
-    let mut transaction = Transaction::new_with_payer(
-        &[Instruction::new_with_bytes(
-            program_id,
-            &instruction_bytes,
-            vec![
-                AccountMeta::new(payer.try_pubkey().unwrap(), false),
-                AccountMeta::new_readonly(rom_id, false),
-                AccountMeta::new(board_id, false),
-            ],
-        )],
-        Some(&payer.try_pubkey().unwrap()),
-    );
-
-    transaction.sign(&[&payer], recent_blockhash);
-    banks_client.process_transaction(transaction).await.unwrap();
-    let new_board_account = banks_client.get_account(board_id).await.unwrap().unwrap();
-    let board_data = new_board_account.data;
-    let new_board: Board = BorshDeserialize::deserialize(&mut board_data.as_slice()).unwrap();
-
     // Composing expected result
     let dummy_card = unsafe { Card::from_raw_parts(0, 1, word_vec![]) };
     let expected_board = unsafe { Board::from_raw_parts(vec![dummy_card], word_vec![], 1) };
 
-    assert_eq!(new_board, expected_board);
+    test(rom, instruction_bytes, board, expected_board).await;
 }
 
 #[tokio::test]
 async fn remove_dummy() {
+    let rom = generate_testing_rom();
+    let dummy_card = unsafe { Card::from_raw_parts(0, 1, word_vec![]) };
+    let board = unsafe { Board::from_raw_parts(vec![dummy_card], word_vec![], 1) };
+
+    // Remove dummy command
+    let mut instruction_bytes = vec![0];
+    let cardtype_index_bytes = u32::to_le_bytes(0);
+    let action_index_bytes = u32::to_le_bytes(1);
+    instruction_bytes.extend_from_slice(&cardtype_index_bytes);
+    instruction_bytes.extend_from_slice(&action_index_bytes);
+
+    // args vector serialization
+    let arg_bytes = word_vec![0].try_to_vec().unwrap();
+    instruction_bytes.extend_from_slice(&arg_bytes);
+
+    // Composing expected result
+    let expected_board = unsafe { Board::from_raw_parts(vec![], word_vec![], 1) };
+
+    test(rom, instruction_bytes, board, expected_board).await;
+}
+
+async fn test(rom: Rom, instruction_bytes: Vec<u8>, board: Board, expected_board: Board) {
     let program_id = Pubkey::new_unique();
     let rom_id = Pubkey::new_unique();
     let board_id = Pubkey::new_unique();
@@ -97,16 +75,12 @@ async fn remove_dummy() {
     let engine_keypair = Keypair::new();
     let engine_id = engine_keypair.try_pubkey().unwrap();
 
-    let rom = generate_testing_rom();
     let rom = rom.try_to_vec().unwrap();
     let mut rom_account = AccountSharedData::new(1_000, 1024, &engine_id);
     rom_account.set_data(rom);
 
-    let dummy_card = unsafe { Card::from_raw_parts(0, 1, word_vec![]) };
-    let board = unsafe { Board::from_raw_parts(vec![dummy_card], word_vec![], 1) };
     let mut board = board.try_to_vec().unwrap();
     board.extend(std::iter::repeat(0).take(24 - board.len()));
-    board.len();
     // Actually board should be also owned by engine, however it is impossible to test it's
     // behavior under solana-program-test (because engine_keypair should be the payer)
     let mut board_account = AccountSharedData::new(1_000, 1024, &program_id);
@@ -118,17 +92,6 @@ async fn remove_dummy() {
 
     let (mut banks_client, payer, recent_blockhash) = program.start().await;
 
-    // Add dummy
-    let mut instruction_bytes = vec![0];
-    let cardtype_index_bytes = u32::to_le_bytes(0);
-    let action_index_bytes = u32::to_le_bytes(1);
-    instruction_bytes.extend_from_slice(&cardtype_index_bytes);
-    instruction_bytes.extend_from_slice(&action_index_bytes);
-
-    // args vector serialization
-    let arg_bytes = word_vec![0].try_to_vec().unwrap();
-    instruction_bytes.extend_from_slice(&arg_bytes);
-
     let mut transaction = Transaction::new_with_payer(
         &[Instruction::new_with_bytes(
             program_id,
@@ -147,9 +110,6 @@ async fn remove_dummy() {
     let new_board_account = banks_client.get_account(board_id).await.unwrap().unwrap();
     let board_data = new_board_account.data;
     let new_board: Board = BorshDeserialize::deserialize(&mut board_data.as_slice()).unwrap();
-
-    // Composing expected result
-    let expected_board = unsafe { Board::from_raw_parts(vec![], word_vec![], 1) };
 
     assert_eq!(new_board, expected_board);
 }
