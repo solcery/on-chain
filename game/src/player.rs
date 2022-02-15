2d62bb8e3f0bbff42f1a49d858e15513d8d45694
Eugene Lomov <eugene.v.lomov@gmail.com>
2022-02-16 00:37:59 +0300

    Moved Player out solcery-game

--- game/src/player.rs
+++ game/src/player.rs
@@ -6,23 +6,9 @@ use solana_program::{
 };
 
 use solana_program::pubkey::Pubkey;
-use std::num::NonZeroU32;
 
-pub const CURRENT_PLAYER_VERSION: u32 = 1;
-
-#[derive(Clone, Eq, PartialEq, Ord, PartialOrd, Hash, Debug, BorshSerialize, BorshDeserialize)]
-//TODO: Add correct Ord implementation
-pub struct Player {
-    pubkey: Pubkey,
-    items: Vec<(u32, Pubkey)>,
-    game: Option<GameInfo>,
-}
-
-#[derive(Clone, Eq, PartialEq, Ord, PartialOrd, Hash, Debug, BorshSerialize, BorshDeserialize)]
-pub struct GameInfo {
-    player_id: NonZeroU32,
-    game_key: Pubkey,
-}
+pub use solcery_data_types::player::Player;
+use solcery_data_types::player::{GameInfo, CURRENT_PLAYER_VERSION};
 
 impl<'r, 's, 't0, 't1> Bundle<'r, 's, 't0, 't1, ()> for Player {
     type Error = Error;
@@ -114,50 +100,6 @@ impl<'r, 's, 't0, 't1> Bundle<'r, 's, 't0, 't1, ()> for Player {
             .serialize(&mut data)
             .map_err(|e| Error::ProgramError(ProgramError::from(e)))
     }
-}
-
-impl Player {
-    #[must_use]
-    pub fn from_pubkey(pubkey: Pubkey) -> Self {
-        Self {
-            pubkey,
-            items: vec![],
-            game: None,
-        }
-    }
-
-    #[must_use]
-    pub fn key(&self) -> Pubkey {
-        self.pubkey
-    }
-
-    #[must_use]
-    pub fn in_game(&self) -> bool {
-        self.game.is_some()
-    }
-
-    #[must_use]
-    pub fn game_key(&self) -> Option<Pubkey> {
-        self.game.as_ref().map(|game| game.game_key)
-    }
-
-    pub unsafe fn set_game(&mut self, game_key: Pubkey, player_id: NonZeroU32) {
-        self.game = Some(GameInfo {
-            player_id,
-            game_key,
-        });
-    }
-
-    pub unsafe fn leave_game(&mut self) {
-        self.game = None;
-    }
-}
-
-#[derive(Clone, Eq, PartialEq, Ord, PartialOrd, Hash, Debug, BorshSerialize, BorshDeserialize)]
-pub struct State {
-    //TODO: move to SolceryPlayer protocol
-    pub pubkey: Pubkey,
-    pub game: Option<Pubkey>,
 }
 
 #[cfg(test)]
