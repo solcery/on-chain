ca344f1e1ff6f4596778aa8911181ee2e6645866
Eugene Lomov <eugene.v.lomov@gmail.com>
2022-03-25 01:00:26 +0300

    Implemented Header methods
    + started implementing tests

--- slice-rbtree/src/lib.rs
+++ slice-rbtree/src/lib.rs
@@ -8,8 +8,8 @@ use bytemuck::{Pod, Zeroable};
 #[repr(C)]
 #[derive(Clone, Copy, Zeroable)]
 struct Node<const KSIZE: usize, const VSIZE: usize> {
-    key: [u8; KSIZE],
-    value: [u8; VSIZE],
+    pub key: [u8; KSIZE],
+    pub value: [u8; VSIZE],
     size: [u8; 4],
     left: [u8; 4],
     right: [u8; 4],
@@ -27,16 +27,12 @@ struct Node<const KSIZE: usize, const VSIZE: usize> {
 unsafe impl<const KSIZE: usize, const VSIZE: usize> Pod for Node<KSIZE, VSIZE> {}
 
 impl<const KSIZE: usize, const VSIZE: usize> Node<KSIZE, VSIZE> {
-    fn key(&self) -> &[u8; KSIZE] {
-        &self.key
-    }
-
-    fn value(&self) -> &[u8; VSIZE] {
-        &self.value
+    fn size(&self) -> u32 {
+        u32::from_be_bytes(self.size)
     }
 
     // TODO: reimplement this functions with macros to avoid code duplication
-    fn left_idx(&self) -> Option<u32> {
+    fn left(&self) -> Option<u32> {
         // bit position of the flag is 0
         if self.flags & 0b0001 == 1 {
             Some(u32::from_be_bytes(self.left))
@@ -45,18 +41,18 @@ impl<const KSIZE: usize, const VSIZE: usize> Node<KSIZE, VSIZE> {
         }
     }
 
-    fn rignt_idx(&self) -> Option<u32> {
+    fn right(&self) -> Option<u32> {
         // bit position of the flag is 1
-        if self.flags & 0b0010 == 1 {
+        if self.flags & 0b0010 != 0 {
             Some(u32::from_be_bytes(self.right))
         } else {
             None
         }
     }
 
-    fn parent_idx(&self) -> Option<u32> {
+    fn parent(&self) -> Option<u32> {
         // bit position of the flag is 2
-        if self.flags & 0b0100 == 1 {
+        if self.flags & 0b0100 != 0 {
             Some(u32::from_be_bytes(self.parent))
         } else {
             None
@@ -64,7 +60,11 @@ impl<const KSIZE: usize, const VSIZE: usize> Node<KSIZE, VSIZE> {
     }
     fn is_red(&self) -> bool {
         // bit position of the flag is 3
-        self.flags & 0b1000 == 1
+        self.flags & 0b1000 != 0
+    }
+
+    unsafe fn set_size(&mut self, size: u32) {
+        self.left = u32::to_be_bytes(size);
     }
 
     unsafe fn set_left(&mut self, left: Option<u32>) {
@@ -89,7 +89,7 @@ impl<const KSIZE: usize, const VSIZE: usize> Node<KSIZE, VSIZE> {
                 self.flags = self.flags | 0b0010;
             }
             None => {
-                // All flags but 1th will remain the same, which will be set to 0.
+                // All flags but 1st will remain the same, which will be set to 0.
                 self.flags = self.flags & 0b1101;
             }
         }
@@ -103,7 +103,7 @@ impl<const KSIZE: usize, const VSIZE: usize> Node<KSIZE, VSIZE> {
                 self.flags = self.flags | 0b0100;
             }
             None => {
-                // All flags but 2th will remain the same, which will be set to 0.
+                // All flags but 2nd will remain the same, which will be set to 0.
                 self.flags = self.flags & 0b1011;
             }
         }
@@ -116,6 +116,142 @@ impl<const KSIZE: usize, const VSIZE: usize> Node<KSIZE, VSIZE> {
             self.flags = self.flags & 0b0111;
         }
     }
+
+    unsafe fn from_raw_parts(
+        key: [u8; KSIZE],
+        value: [u8; VSIZE],
+        size: u32,
+        left: Option<u32>,
+        right: Option<u32>,
+        parent: Option<u32>,
+        is_red: bool,
+    ) -> Self {
+        let size = u32::to_be_bytes(size);
+        let mut flags = 0b00;
+
+        let left = match left {
+            Some(index) => {
+                flags = flags | 0b0001;
+                u32::to_be_bytes(index)
+            }
+            None => u32::to_be_bytes(0),
+        };
+
+        let right = match right {
+            Some(index) => {
+                flags = flags | 0b0010;
+                u32::to_be_bytes(index)
+            }
+            None => u32::to_be_bytes(0),
+        };
+
+        let parent = match parent {
+            Some(index) => {
+                flags = flags | 0b0100;
+                u32::to_be_bytes(index)
+            }
+            None => u32::to_be_bytes(0),
+        };
+
+        if is_red {
+            flags = flags | 0b1000;
+        }
+
+        Self {
+            key,
+            value,
+            size,
+            left,
+            right,
+            parent,
+            flags,
+        }
+    }
+}
+
+#[cfg(test)]
+mod node_tests {
+    use super::*;
+    use pretty_assertions::assert_eq;
+
+    #[test]
+    fn left() {
+        let mut node =
+            unsafe { Node::<1, 1>::from_raw_parts([1], [2], 3, None, None, None, false) };
+        assert_eq!(node.left(), None);
+
+        unsafe {
+            node.set_left(Some(1));
+        }
+        assert_eq!(node.left(), Some(1));
+
+        unsafe {
+            node.set_left(Some(2));
+        }
+        assert_eq!(node.left(), Some(2));
+
+        // check other values
+
+        assert_eq!(node.key, [1]);
+        assert_eq!(node.value, [2]);
+        assert_eq!(node.size(), 3);
+        assert_eq!(node.right(), None);
+        assert_eq!(node.parent(), None);
+        assert_eq!(node.is_red(), false);
+    }
+
+    #[test]
+    fn right() {
+        let mut node =
+            unsafe { Node::<1, 1>::from_raw_parts([1], [2], 3, None, None, None, false) };
+        assert_eq!(node.right(), None);
+
+        unsafe {
+            node.set_right(Some(1));
+        }
+        dbg!(&node.right);
+        dbg!(&node.flags);
+        assert_eq!(node.right(), Some(1));
+
+        unsafe {
+            node.set_right(Some(2));
+        }
+        assert_eq!(node.right(), Some(2));
+
+        // check other values
+
+        assert_eq!(node.key, [1]);
+        assert_eq!(node.value, [2]);
+        assert_eq!(node.size(), 3);
+        assert_eq!(node.left(), None);
+        assert_eq!(node.parent(), None);
+        assert_eq!(node.is_red(), false);
+    }
+    #[test]
+    fn parent() {
+        let mut node =
+            unsafe { Node::<1, 1>::from_raw_parts([1], [2], 3, None, None, None, false) };
+        assert_eq!(node.parent(), None);
+
+        unsafe {
+            node.set_parent(Some(1));
+        }
+        assert_eq!(node.parent(), Some(1));
+
+        unsafe {
+            node.set_parent(Some(2));
+        }
+        assert_eq!(node.parent(), Some(2));
+
+        // check other values
+
+        assert_eq!(node.key, [1]);
+        assert_eq!(node.value, [2]);
+        assert_eq!(node.size(), 3);
+        assert_eq!(node.right(), None);
+        assert_eq!(node.right(), None);
+        assert_eq!(node.is_red(), false);
+    }
 }
 
 #[repr(C)]
@@ -125,8 +261,110 @@ struct Header {
     v_size: [u8; 4],
     max_nodes: [u8; 4],
     root: [u8; 4],
-    is_root_present: u8,
-    empty_node_ptr: [u8; 4],
+    /// Flag layout:
+    ///
+    /// 0. is_root_present
+    /// 1. is_head_present
+    flags: u8,
+    /// head of the linked list of empty nodes
+    head: [u8; 4],
+}
+
+impl Header {
+    fn k_size(&self) -> u32 {
+        u32::from_be_bytes(self.k_size)
+    }
+
+    fn v_size(&self) -> u32 {
+        u32::from_be_bytes(self.v_size)
+    }
+
+    fn max_nodes(&self) -> u32 {
+        u32::from_be_bytes(self.max_nodes)
+    }
+
+    fn root_index(&self) -> Option<u32> {
+        // bit position of the flag is 0
+        if self.flags & 0b0001 == 1 {
+            Some(u32::from_be_bytes(self.root))
+        } else {
+            None
+        }
+    }
+
+    fn head_index(&self) -> Option<u32> {
+        // bit position of the flag is 1
+        if self.flags & 0b0010 == 1 {
+            Some(u32::from_be_bytes(self.head))
+        } else {
+            None
+        }
+    }
+
+    unsafe fn set_root(&mut self, root: Option<u32>) {
+        // bit position of the flag is 0
+        match root {
+            Some(idx) => {
+                self.root = u32::to_be_bytes(idx);
+                self.flags = self.flags | 0b0001;
+            }
+            None => {
+                // All flags but 0th will remain the same, which will be set to 0.
+                self.flags = self.flags & 0b1110;
+            }
+        }
+    }
+
+    unsafe fn set_head(&mut self, head: Option<u32>) {
+        // bit position of the flag is 1
+        match head {
+            Some(idx) => {
+                self.head = u32::to_be_bytes(idx);
+                self.flags = self.flags | 0b0010;
+            }
+            None => {
+                // All flags but 1st will remain the same, which will be set to 0.
+                self.flags = self.flags & 0b1101;
+            }
+        }
+    }
+
+    unsafe fn from_raw_parts(
+        k_size: u32,
+        v_size: u32,
+        max_nodes: u32,
+        root: Option<u32>,
+        head: Option<u32>,
+    ) -> Self {
+        let k_size = u32::to_be_bytes(k_size);
+        let v_size = u32::to_be_bytes(v_size);
+        let max_nodes = u32::to_be_bytes(max_nodes);
+        let mut flags = 0b00;
+
+        let root = match root {
+            Some(index) => {
+                flags = flags | 0b01;
+                u32::to_be_bytes(index)
+            }
+            None => u32::to_be_bytes(0),
+        };
+
+        let head = match head {
+            Some(index) => {
+                flags = flags | 0b10;
+                u32::to_be_bytes(index)
+            }
+            None => u32::to_be_bytes(0),
+        };
+        Self {
+            k_size,
+            v_size,
+            max_nodes,
+            root,
+            head,
+            flags,
+        }
+    }
 }
 
 pub struct RBtree<'a, K, V, const KSIZE: usize, const VSIZE: usize>
@@ -138,15 +376,4 @@ where
     nodes: &'a mut [Node<KSIZE, VSIZE>],
     _phantom_key: PhantomData<K>,
     _phantom_value: PhantomData<V>,
-}
-
-#[cfg(test)]
-mod tests {
-    use super::*;
-    use std::mem;
-
-    #[test]
-    fn alignment_node() {
-        assert_eq!(mem::align_of::<Node<8, 8>>(), 1);
-    }
 }
