81470be25256d289fd9630183df7060cd136f219
Eugene Lomov <eugene.v.lomov@gmail.com>
2022-03-25 12:40:55 +0300

    Added allocation routines for RBTree

--- slice-rbtree/src/lib.rs
+++ slice-rbtree/src/lib.rs
@@ -1,8 +1,9 @@
 use borsh::{BorshDeserialize, BorshSerialize};
-use bytemuck::{Pod, Zeroable};
+use bytemuck::{cast_mut, cast_slice_mut, Pod, Zeroable};
 use std::cmp::Ord;
 use std::cmp::Ordering;
 use std::marker::PhantomData;
+use std::mem;
 
 #[repr(C)]
 #[derive(Clone, Copy, Zeroable)]
@@ -327,6 +328,25 @@ impl Header {
             flags,
         }
     }
+
+    /// This function guarantees, that the header will be initialized in fully known state
+    unsafe fn fill(
+        &mut self,
+        k_size: u32,
+        v_size: u32,
+        max_nodes: u32,
+        root: Option<u32>,
+        head: Option<u32>,
+    ) {
+        self.k_size = u32::to_be_bytes(k_size);
+        self.v_size = u32::to_be_bytes(v_size);
+        self.max_nodes = u32::to_be_bytes(max_nodes);
+        self.flags = 0b00;
+        self.set_head(head);
+        self.set_root(root);
+    }
+}
+
 #[cfg(test)]
 mod header_tests {
     use super::*;
@@ -380,4 +400,90 @@ where
     nodes: &'a mut [Node<KSIZE, VSIZE>],
     _phantom_key: PhantomData<K>,
     _phantom_value: PhantomData<V>,
+}
+
+impl<'a, K, V, const KSIZE: usize, const VSIZE: usize> RBtree<'a, K, V, KSIZE, VSIZE>
+where
+    K: Ord + BorshDeserialize + BorshSerialize,
+    V: BorshDeserialize + BorshSerialize,
+{
+    pub fn init_slice(slice: &'a mut [u8]) -> Result<Self, Error> {
+        if slice.len() <= mem::size_of::<Header>() {
+            return Err(Error::TooSmall);
+        }
+
+        let (header, nodes) = slice.split_at_mut(mem::size_of::<Header>());
+
+        if nodes.len() == 0 {
+            return Err(Error::TooSmall);
+        }
+
+        if nodes.len() % mem::size_of::<Node<KSIZE, VSIZE>>() != 0 {
+            return Err(Error::WrongNodePoolSize);
+        }
+        let nodes: &mut [Node<KSIZE, VSIZE>] = cast_slice_mut(nodes);
+        let header: &mut [[u8; mem::size_of::<Header>()]] = cast_slice_mut(header);
+        let header: &mut Header = cast_mut(&mut header[0]);
+
+        unsafe {
+            // Allocator initialization
+            nodes[0].set_parent(None);
+
+            for (i, node) in nodes.iter_mut().enumerate().skip(1) {
+                node.set_parent(Some((i - 1) as u32));
+            }
+
+            header.fill(
+                KSIZE as u32,
+                VSIZE as u32,
+                nodes.len() as u32,
+                None,
+                Some((nodes.len() - 1) as u32),
+            );
+        }
+        Ok(Self {
+            header,
+            nodes,
+            _phantom_key: PhantomData::<K>,
+            _phantom_value: PhantomData::<V>,
+        })
+    }
+
+    /// Deallocates a node
+    ///
+    /// # Safety
+    ///
+    /// This function does nothing but deallocation. It should be checked, that the node is
+    /// completely unlinked from the tree.
+    unsafe fn delete_node(&mut self, index: usize) {
+        let allocator_head = self.header.head();
+        let node_index = Some(index as u32);
+
+        self.nodes[index].set_parent(allocator_head);
+        self.header.set_head(node_index);
+    }
+
+    /// Allocates a node
+    ///
+    /// # Safety
+    ///
+    /// This function does nothing but allocation. The returned node (if present) is
+    /// completely unlinked from the tree and is in the unknown state. The caller must fill the
+    /// node with correct data.
+    unsafe fn allocate_node(&mut self) -> Option<usize> {
+        let allocator_head = self.header.head();
+        match allocator_head {
+            Some(index) => {
+                let new_head = self.nodes[index as usize].parent();
+                self.header.set_head(new_head);
+                Some(index as usize)
+            }
+            None => None,
+        }
+    }
+}
+
+pub enum Error {
+    TooSmall,
+    WrongNodePoolSize,
 }
