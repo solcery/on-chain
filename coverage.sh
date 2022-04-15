9b46452ad1d53dea5746da1e3e4639475059716a
Eugene Lomov <eugene.v.lomov@gmail.com>
2022-04-15 14:16:37 +0300

    Fixed toolchains in CI

--- coverage.sh
+++ coverage.sh
@@ -3,9 +3,9 @@ export CARGO_INCREMENTAL=0
 export RUSTFLAGS="-Zprofile -Ccodegen-units=1 -Copt-level=0 -Clink-dead-code -Coverflow-checks=off -Zpanic_abort_tests -Cpanic=abort"
 export RUSTDOCFLAGS="-Cpanic=abort"
 
-cargo +nightly build
+cargo build
 
-cargo +nightly test
+cargo test
 
 grcov . -s . --binary-path ./target/debug/ -t html --branch --ignore-not-existing -o ./target/debug/coverage/
 
