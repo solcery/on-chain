3fe171421d148ea98593c277c8913b6c29b9b909
Alexey Belyakov <qqrm@solcery.xyz>
2022-04-18 03:08:06 +0300

    Added unimplemented schemas manager

---
+++ db-manager/src/db_manager.rs
@@ -0,0 +1,13 @@
+pub struct DBManager {}
+
+impl DBManager {
+    pub fn create_db() {
+        unimplemented!();
+    }
+    pub fn remove_db() {
+        unimplemented!();
+    }
+    pub fn process_request() {
+        unimplemented!();
+    }
+}
--- db-manager/src/lib.rs
+++ db-manager/src/lib.rs
@@ -1,4 +1,6 @@
 #![deny(missing_docs)]
 
+pub mod db_manager;
 mod entrypoint;
 pub mod processor;
+pub mod schemas_manager;
