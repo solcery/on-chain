ddb0865e4057d2f4ca82f2126973b4161ba73ec7
Alexey Belyakov <qqrm@solcery.xyz>
2022-04-18 20:44:36 +0300

    Added base erros

--- db-manager/src/schemas_manager.rs
+++ db-manager/src/schemas_manager.rs
@@ -1,32 +1,39 @@
+use self::error::SchemasManagerError;
 use borsh::{BorshDeserialize, BorshSerialize};
-use slice_rbtree::RBTree;
+// use slice_rbtree::RBTree;
+
+mod error;
 
 //TODO: fill allowed types
-enum AllowedTypes {}
+enum _AllowedTypes {}
 
+#[allow(missing_docs)]
 #[derive(Clone, Eq, PartialEq, Ord, PartialOrd, Debug, BorshSerialize, BorshDeserialize)]
 pub struct Schema {
     pub version: u64,
-    tables: String, //TODO: how store tables order/types?
+    tables: String, // TODO: how store tables order/types?
 }
 
 pub type SchemaId = String;
-
 pub struct SchemasManager {
     // storage: RBTree,
 }
 
+#[allow(missing_docs)]
 impl SchemasManager {
-    pub fn add_schema() {
+    pub fn add_schema(_schema_id: SchemaId, _schema: Schema) -> Result<(), SchemasManagerError> {
         unimplemented!();
     }
-    pub fn remove_schema() {
+    pub fn remove_schema(_id: SchemaId) -> Result<(), SchemasManagerError> {
         unimplemented!();
     }
-    pub fn update_schema() {
+    pub fn update_schema(
+        _schema_id: SchemaId,
+        _new_schema: Schema,
+    ) -> Result<(), SchemasManagerError> {
         unimplemented!();
     }
-    pub fn get_schema(id: SchemaId) {
+    pub fn get_schema(_id: SchemaId) -> Result<(), SchemasManagerError> {
         unimplemented!();
     }
 }
