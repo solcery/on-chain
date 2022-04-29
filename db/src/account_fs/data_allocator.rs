c93118c9b501cde29e5bfa1c33d224767ddc5b67
Eugene Lomov <eugene.v.lomov@gmail.com>
2022-04-29 18:01:41 +0300

    Fixed UB in DataAllocator found via Miri

--- db/src/account_fs/data_allocator.rs
+++ db/src/account_fs/data_allocator.rs
@@ -1,31 +1,34 @@
-use super::AccountAllocator;
 use core::slice::from_raw_parts_mut;
 use solana_program::pubkey::Pubkey;
 use std::collections::{BTreeMap, BTreeSet};
 use std::fmt;
+use std::marker::PhantomData;
+use std::ptr::NonNull;
 
-type SegmentOffset = (usize, usize);
+type Segment = (usize, usize);
 
 pub struct DataAllocator<'long> {
     pubkey: Pubkey,
-    data: &'long mut [u8],
-    allocated_segments: BTreeMap<u32, SegmentOffset>,
+    ptr: NonNull<u8>,
+    len: usize,
+    allocated_segments: BTreeMap<u32, Segment>,
     borrowed_serments: BTreeSet<u32>,
+    _ghost: PhantomData<&'long mut [u8]>,
 }
 
-impl<'a> fmt::Debug for DataAllocator<'a> {
+impl<'long> fmt::Debug for DataAllocator<'long> {
     fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
         f.debug_struct("DataAllocator")
             .field("pubkey", &self.pubkey)
             .field("allocated_segments", &self.allocated_segments)
             .field("borrowed_serments", &self.borrowed_serments)
-            .field("data_size", &self.data.len())
+            .field("data_size", &self.len)
             .finish()
     }
 }
 
-impl<'a> DataAllocator<'a> {
-    pub fn segment(&mut self, id: u32) -> Result<&'a mut [u8], DataError> {
+impl<'long: 'short, 'short> DataAllocator<'long> {
+    pub fn segment(&mut self, id: u32) -> Result<&'short mut [u8], DataError> {
         if self.borrowed_serments.contains(&id) {
             return Err(DataError::AlreadyBorrowed);
         }
@@ -34,12 +37,41 @@ impl<'a> DataAllocator<'a> {
             self.borrowed_serments.insert(id);
 
             unsafe {
-                //TODO: document safety
-                let data_ptr = self.data.as_mut_ptr();
+                // Safety
+                //
+                // Safety contract of `from_raw_parts_mut`
+                // * `data` must be valid for both reads and writes for `len * mem::size_of::<T>()` many bytes,
+                //   and it must be properly aligned. This means in particular:
+                //   -- Check: the emitted slice has the lifetime 'short which is not longer than 'long.
+                //
+                //     * The entire memory range of this slice must be contained within a single allocated object!
+                //       Slices can never span across multiple allocated objects.
+                //       -- Check: we are offsetting inside one big slice.
+                //
+                //     * `data` must be non-null and aligned even for zero-length slices. One
+                //       reason for this is that enum layout optimizations may rely on references
+                //       (including slices of any length) being aligned and non-null to distinguish
+                //       them from other data. You can obtain a pointer that is usable as `data`
+                //       for zero-length slices using [`NonNull::dangling()`].
+                //       -- Check: data_ptr is not null and so does slice_ptr.
+                //
+                // * `data` must point to `len` consecutive properly initialized values of type `T`.
+                //    -- Check: the original slice of bytes was initialized,
+                //       offset bounds was checked to be inside the original slice.
+                //
+                // * The memory referenced by the returned slice must not be accessed through any other pointer
+                //   (not derived from the return value) for the duration of lifetime `'short`.
+                //   Both read and write accesses are forbidden.
+                //   -- Check: self.borrowed_segments guarantees, that this segment will not be emitted again.
+                //      Segments was checked to be non-overlapping.
+                //
+                // * The total size `len * mem::size_of::<T>()` of the slice must be no larger than `isize::MAX`.
+                //   -- Check: here we are limited by the maximum account size, which is far less than `isize::MAX`.
 
-                debug_assert!(offset_end <= self.data.len());
+                debug_assert!(offset_end <= self.len);
                 debug_assert!(offset_start < offset_end);
 
+                let data_ptr = self.ptr.as_ptr();
                 let len = offset_end - offset_start;
                 let slice_ptr = data_ptr.add(offset_start);
 
@@ -52,20 +84,30 @@ impl<'a> DataAllocator<'a> {
 
     pub(super) unsafe fn from_raw_parts(
         pubkey: Pubkey,
-        data: &'a mut [u8],
-        allocated_segments: BTreeMap<u32, SegmentOffset>,
+        data: &'long mut [u8],
+        allocated_segments: BTreeMap<u32, Segment>,
         borrowed_serments: BTreeSet<u32>,
     ) -> Self {
+        let len = data.len();
+        let ptr = NonNull::new(data.as_mut_ptr()).unwrap();
         Self {
             pubkey,
-            data,
+            len,
+            ptr,
             allocated_segments,
             borrowed_serments,
+            _ghost: PhantomData::<&'long mut [u8]>,
         }
     }
 }
 
+#[derive(Debug, PartialEq, Eq, Copy, Clone)]
 pub enum DataError {
     NoSuchSegment,
     AlreadyBorrowed,
 }
+
+#[cfg(test)]
+use super::AccountAllocator;
+#[cfg(test)]
+mod tests;
