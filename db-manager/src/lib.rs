721a62103605a1833b1d4a13cd12cf34239a100b
Eugene Lomov <eugene.v.lomov@gmail.com>
2022-05-07 19:35:09 +0300

    Fixed db-manager  build and bumped deps

--- db-manager/src/lib.rs
+++ db-manager/src/lib.rs
@@ -1,6 +1,8 @@
 // #![deny(missing_docs)] // TODO: enable and fill docs
 
 pub mod db_manager;
-mod entrypoint;
 pub mod error;
 pub mod processor;
+
+#[cfg(not(feature = "no-entrypoint"))]
+mod entrypoint;
