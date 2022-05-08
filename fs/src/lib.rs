36dd2d7054a583d8e2d9207eb2a5de675e4c56eb
Eugene Lomov <eugene.v.lomov@gmail.com>
2022-05-08 12:09:46 +0300

    Implemented AccountFS

--- fs/src/lib.rs
+++ fs/src/lib.rs
@@ -4,78 +4,31 @@
 //!
 //! Each account used in the DB has the following layout:
 //!
-//! First 33 bytes contain [AllocationTable](account_allocator::AllocationTable) struct, then goes
-//! [Inode](account_allocator::Inode) table with `inodes_max` elements. Size of each Inode is 13
-//! bytes. All the remaining space is usable for data.
+//! * First 33 bytes contain [AllocationTable](account_allocator::AllocationTable) struct
+//! * then goes [Inode](account_allocator::Inode) table with `inodes_max` elements. Size of each
+//! Inode is 13 bytes.
+//! * All the remaining space is usable for data.
+//!
+//! Operation with FS is splitted into two stages:
+//! - First stage is represented by [FSAllocator]. At this stage you can allocate and deallocate
+//! segments of memory.
+//! - Second stage is represented by [FSDispatcher], which can only be condtructed from
+//! [FSAllocator]. At this stage you can get mutable slices to allocated memory segments.
 
 #![deny(unsafe_op_in_unsafe_fn)]
 #![feature(cell_leak)]
-use solana_program::{
-    account_info::next_account_info, account_info::AccountInfo, program_error::ProgramError,
-    pubkey::Pubkey,
-};
-use std::cell::RefMut;
-use std::collections::BTreeMap;
 
 mod account_allocator;
 mod data_allocator;
+mod fs_allocator;
+mod fs_dispatcher;
 mod segment_id;
 
-pub use account_allocator::{AccountAllocator, Error as AllocatorError};
-pub use data_allocator::DataAllocator;
+use account_allocator::AccountAllocator;
+use data_allocator::DataAllocator;
+
+pub use account_allocator::Error as AllocatorError;
+pub use data_allocator::DataError;
+pub use fs_allocator::FSAllocator;
+pub use fs_dispatcher::FSDispatcher;
 pub use segment_id::SegmentId;
-
-#[derive(Debug)]
-pub struct FSAllocator<'a> {
-    allocators: BTreeMap<Pubkey, AccountAllocator<'a>>,
-}
-
-impl<'a> FSAllocator<'a> {
-    /// Constructs [FSAllocator], assuming that all account are initialized as filesystem accounts
-    pub fn from_account_iter<AccountIter>(
-        accounts_iter: &mut AccountIter,
-    ) -> Result<Self, AllocatorError>
-    where
-        AccountIter: Iterator<Item = &'a AccountInfo<'a>>,
-    {
-        let result: Result<BTreeMap<Pubkey, AccountAllocator<'a>>, _> = accounts_iter
-            .map(|account| {
-                let pubkey = *account.key;
-                let data = account.data.borrow_mut();
-                let data = RefMut::<'_, &'a mut [u8]>::leak(data);
-                unsafe { AccountAllocator::from_account(data).map(|alloc| (pubkey, alloc)) }
-            })
-            .collect();
-
-        result.map(|allocators| Self { allocators })
-    }
-
-    /// Constructs [FSAllocator], assuming that some (or any) accounts may be uninitialized as filesystem accounts
-    pub fn from_uninit_account_iter<AccountIter>(
-        accounts_iter: &mut AccountIter,
-    ) -> Result<Self, AllocatorError>
-    where
-        AccountIter: Iterator<Item = &'a AccountInfo<'a>>,
-    {
-        //let result: Result<BTreeMap<Pubkey, AccountAllocator<'a>>, _> = accounts_iter
-        //.map(|account| {
-        //let pubkey = *account.key;
-        //let data = account.data.borrow_mut();
-        //let data = RefMut::<'_, &'a mut [u8]>::leak(data);
-        //let maybe_alloc = unsafe { AccountAllocator::from_account(*data) };
-
-        //// We can't use match here because of borrow checker
-        //if let Ok(alloc) = maybe_alloc {
-        //return Ok((pubkey, alloc));
-        //} else if maybe_alloc.unwrap_err() == AllocatorError::TooSmall {
-        //Err(AllocatorError::TooSmall)
-        //} else {
-        //unsafe { AccountAllocator::from_account(*data).map(|alloc| (pubkey, alloc)) }
-        //}
-        //})
-        //.collect();
-
-        //result.map(|allocators| Self { allocators })
-        unimplemented!(); // Thanks to Borrow Checker
-    }
-}
--- fs/src/segment_id.rs
+++ fs/src/segment_id.rs
@@ -1,6 +1,6 @@
 use solana_program::pubkey::Pubkey;
 
-#[derive(Debug, Eq, PartialEq, Ord, PartialOrd)]
+#[derive(Debug, Eq, PartialEq, Ord, PartialOrd, Copy, Clone)]
 pub struct SegmentId {
     pub pubkey: Pubkey,
     pub id: u32,
