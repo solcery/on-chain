ceab5d6a370b5d3dfa75ef56879bf672b9318fe3
Eugene Lomov <eugene.v.lomov@gmail.com>
2022-05-09 20:39:18 +0300

    Reorganized files in db/

--- db/src/lib.rs
+++ db/src/lib.rs
@@ -1,5 +1,11 @@
 //! Solcery DB internal structures
 #![deny(unsafe_op_in_unsafe_fn)]
-mod column;
-mod enums;
-mod index;
+mod raw;
+
+use account_fs::{FSAllocator, FSDispatcher};
+use raw::column::Column;
+use raw::index::Index;
+
+pub struct DBManager<'a> {
+    fs: &mut FSDispatcher<'a>,
+}
