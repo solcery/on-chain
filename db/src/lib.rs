9ee64596011d988478c73a44789fb79ec2101829
Eugene Lomov <eugene.v.lomov@gmail.com>
2022-05-11 18:11:50 +0300

    Added Column trait

--- db/src/lib.rs
+++ db/src/lib.rs
@@ -1,11 +1,25 @@
 //! Solcery DB internal structures
 #![deny(unsafe_op_in_unsafe_fn)]
+
+use std::collections::BTreeMap;
+
+mod column;
 mod raw;
 
-use account_fs::{FSAllocator, FSDispatcher};
-use raw::column::Column;
+use account_fs::FS;
+use column::Column;
+use raw::column::Column as RawColumn;
+use raw::enums::{ColumnType, Data, DataType};
 use raw::index::Index;
 
 pub struct DBManager<'a> {
-    fs: &mut FSDispatcher<'a>,
+    fs: &'a mut FS<'a>,
+    databases: BTreeMap<String, DB<'a>>,
+}
+
+pub struct DB<'a> {
+    index: &'a mut Index,
+    columns: BTreeMap<String, &'a mut dyn Column>,
+    // Do we need on-the-fly column bootstrapping?
+    // It will speed up DB loading.
 }
