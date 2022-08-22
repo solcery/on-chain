02b64afac3da8be75e2a6d7604aea5f4a48d5229
Eugene Lomov <eugene.v.lomov@gmail.com>
2022-08-22 21:27:39 +0300

    Moved fs_test functionality to separate crate

---
+++ fs/fs_test/src/lib.rs
@@ -0,0 +1,53 @@
+//! A small colection of utilities used for testing code with account-fs
+
+use account_fs::FS;
+use solana_program::{account_info::AccountInfo, pubkey::Pubkey};
+
+#[derive(Clone, Debug, Eq, PartialEq)]
+pub struct AccountParams {
+    pub owner: Pubkey,
+    pub data: Data,
+    pub is_signer: bool,
+    pub is_writable: bool,
+}
+
+/// This struct is used to store data, which is borrowed in ordinal AccountInfo<'_>
+#[derive(Clone, Debug, Eq, PartialEq)]
+pub struct InternalAccountInfo {
+    key: Pubkey,
+    lamports: u64,
+    data: Vec<u8>,
+    owner: Pubkey,
+}
+
+#[derive(Clone, Debug, Eq, PartialEq)]
+pub enum Data {
+    Filled(Vec<u8>),
+    Empty(usize),
+}
+
+pub fn prepare_account_info(params: AccountParams) -> AccountInfo<'static> {
+    let data = match params.data {
+        Data::Filled(vec) => vec,
+        Data::Empty(cap) => vec![0; cap],
+    };
+
+    let internal_info: &'static mut InternalAccountInfo =
+        Box::leak(Box::new(InternalAccountInfo {
+            key: Pubkey::new_unique(),
+            lamports: 1,
+            data,
+            owner: params.owner,
+        }));
+
+    AccountInfo::new(
+        &internal_info.key,
+        params.is_signer,
+        params.is_writable,
+        &mut internal_info.lamports,
+        &mut internal_info.data,
+        &internal_info.owner,
+        false,
+        1,
+    )
+}
