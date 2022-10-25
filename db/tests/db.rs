13f403a4dbdefdc71a121e0769d4374040500680
Eugene Lomov <eugene.v.lomov@gmail.com>
2022-10-25 21:00:46 +0300

    Added more tests to solcery-db

--- db/tests/db.rs
+++ db/tests/db.rs
@@ -11,125 +11,6 @@ use fs_test::*;
 use solcery_db::*;
 
 #[test]
-fn values() {
-    let program_id = Pubkey::new_unique();
-
-    let account_params = AccountParams {
-        address: None,
-        owner: program_id,
-        data: AccountData::Empty(10_000),
-    };
-    let mut fs_data = FSAccounts::replicate_params(account_params, 3);
-
-    let account_infos = fs_data.account_info_iter();
-    let fs = Rc::new(RefCell::new(
-        FS::from_uninit_account_iter(&program_id, &mut account_infos.iter(), 10).unwrap(),
-    ));
-
-    let table_name = "Test DB";
-    let max_columns = 12;
-    let max_rows = 53;
-    let primary_key_type = DataType::ShortString;
-    let (mut db, segment) = DB::init_in_segment(
-        fs.clone(),
-        table_name,
-        max_columns,
-        max_rows,
-        primary_key_type,
-    )
-    .unwrap();
-
-    let column_name = "Test Column";
-    let dtype = DataType::Int;
-    let col_id = db.add_column(column_name, dtype, false).unwrap();
-
-    let value = Data::Int(123);
-    let value2 = Data::Int(125);
-    let primary_key = Data::ShortString("test".to_string());
-    let old_val = db
-        .set_value(primary_key.clone(), col_id, value.clone())
-        .unwrap();
-    assert_eq!(old_val, None);
-    let new_val = db
-        .set_value(primary_key.clone(), col_id, value2.clone())
-        .unwrap();
-    assert_eq!(new_val, Some(value));
-
-    drop(db);
-
-    let mut db = DB::from_segment(fs.clone(), segment).unwrap();
-
-    let obtained_val = db.value(primary_key.clone(), col_id).unwrap();
-    assert_eq!(obtained_val, Some(value2));
-    db.delete_value(primary_key.clone(), col_id).unwrap();
-
-    let no_value = db.value(primary_key, col_id).unwrap();
-    assert_eq!(no_value, None);
-}
-
-#[test]
-fn secondary_key() {
-    let program_id = Pubkey::new_unique();
-
-    let account_params = AccountParams {
-        address: None,
-        owner: program_id,
-        data: AccountData::Empty(10_000),
-    };
-    let mut fs_data = FSAccounts::replicate_params(account_params, 3);
-
-    let account_infos = fs_data.account_info_iter();
-    let fs = Rc::new(RefCell::new(
-        FS::from_uninit_account_iter(&program_id, &mut account_infos.iter(), 10).unwrap(),
-    ));
-
-    let table_name = "Test DB";
-    let max_columns = 12;
-    let max_rows = 53;
-    let primary_key_type = DataType::ShortString;
-    let (mut db, _) = DB::init_in_segment(
-        fs.clone(),
-        table_name,
-        max_columns,
-        max_rows,
-        primary_key_type,
-    )
-    .unwrap();
-
-    let column_name = "Test Column";
-    let dtype = DataType::Int;
-    let col_id = db.add_column(column_name, dtype, false).unwrap();
-
-    let value = Data::Int(123);
-    let primary_key = Data::ShortString("test".to_string());
-    let old_val = db
-        .set_value(primary_key.clone(), col_id, value.clone())
-        .unwrap();
-    assert_eq!(old_val, None);
-
-    let column_name = "Secondary Key Column";
-    let dtype = DataType::Int;
-    let key_col_id = db.add_column(column_name, dtype, true).unwrap();
-
-    let secondary_value = Data::Int(15);
-    let old_val = db
-        .set_value(primary_key.clone(), key_col_id, secondary_value.clone())
-        .unwrap();
-    assert_eq!(old_val, None);
-
-    let obtained_val = db
-        .value_secondary(key_col_id, secondary_value.clone(), col_id)
-        .unwrap();
-    assert_eq!(obtained_val, Some(value));
-
-    db.delete_value_secondary(key_col_id, secondary_value, col_id)
-        .unwrap();
-
-    let no_value = db.value(primary_key, col_id).unwrap();
-    assert_eq!(no_value, None);
-}
-
-#[test]
 fn creation_of_the_test_db() {
     let filename = format!("{}/tests/fs_images/clean_fs", env!("CARGO_MANIFEST_DIR"));
 
@@ -276,6 +157,276 @@ fn remove_column() {
 
     let age_column = ColumnId::new(1);
     db.remove_column(age_column).unwrap();
+
+    let err = db.value(Data::Int(0), ColumnId::new(0)).unwrap_err();
+    assert_eq!(err, Error::NoSuchColumn);
+
+    let err = db.value(Data::Int(0), ColumnId::new(1)).unwrap_err();
+    assert_eq!(err, Error::NoSuchColumn);
+}
+
+#[test]
+fn set_value() {
+    let filename = format!("{}/tests/fs_images/prepared_db", env!("CARGO_MANIFEST_DIR"));
+
+    let mut file = File::open(filename).unwrap();
+
+    let mut db_fs_bytes = Vec::new();
+    file.read_to_end(&mut db_fs_bytes).unwrap();
+
+    let mut fs_data = FSAccounts::deserialize(&mut db_fs_bytes.as_slice()).unwrap();
+
+    let program_id = fs_data.owner_pubkey().unwrap();
+
+    let account_infos = fs_data.account_info_iter();
+    let fs = Rc::new(RefCell::new(
+        FS::from_account_iter(&program_id, &mut account_infos.iter()).unwrap(),
+    ));
+
+    let mut db = DB::from_segment(fs.clone(), DB_SEGMENT).unwrap();
+
+    let name_column = ColumnId::new(0);
+
+    let old_val = db
+        .set_value(
+            Data::Int(5),
+            name_column,
+            Data::ShortString("Alec".to_string()),
+        )
+        .unwrap();
+    assert_eq!(old_val, None);
+
+    let old_val = db
+        .set_value(
+            Data::Int(5),
+            name_column,
+            Data::ShortString("Alex".to_string()),
+        )
+        .unwrap();
+    assert_eq!(old_val, Some(Data::ShortString("Alec".to_string())));
+
+    let name_column = ColumnId::new(1);
+
+    let old_val = db.value(Data::Int(5), name_column).unwrap();
+    assert_eq!(old_val, None);
+
+    let old_val = db
+        .set_value(Data::Int(5), name_column, Data::Int(18))
+        .unwrap();
+    assert_eq!(old_val, None);
+
+    let old_val = db
+        .set_value(Data::Int(5), name_column, Data::Int(19))
+        .unwrap();
+    assert_eq!(old_val, Some(Data::Int(18)));
+}
+
+#[test]
+fn set_value_secondary() {
+    let filename = format!("{}/tests/fs_images/prepared_db", env!("CARGO_MANIFEST_DIR"));
+
+    let mut file = File::open(filename).unwrap();
+
+    let mut db_fs_bytes = Vec::new();
+    file.read_to_end(&mut db_fs_bytes).unwrap();
+
+    let mut fs_data = FSAccounts::deserialize(&mut db_fs_bytes.as_slice()).unwrap();
+
+    let program_id = fs_data.owner_pubkey().unwrap();
+
+    let account_infos = fs_data.account_info_iter();
+    let fs = Rc::new(RefCell::new(
+        FS::from_account_iter(&program_id, &mut account_infos.iter()).unwrap(),
+    ));
+
+    let mut db = DB::from_segment(fs.clone(), DB_SEGMENT).unwrap();
+
+    let name_column = ColumnId::new(0);
+    let age_column = ColumnId::new(1);
+
+    let err = db
+        .set_value_secondary(
+            name_column,
+            Data::ShortString("Alex".to_string()),
+            age_column,
+            Data::Int(20),
+        )
+        .unwrap_err();
+    assert_eq!(err, Error::SecondaryKeyWithNonExistentPrimaryKey);
+
+    let old_val = db
+        .set_value_secondary(
+            name_column,
+            Data::ShortString("Alice".to_string()),
+            age_column,
+            Data::Int(20),
+        )
+        .unwrap();
+    assert_eq!(old_val, Some(Data::Int(22)));
+}
+
+#[test]
+fn value() {
+    let filename = format!("{}/tests/fs_images/prepared_db", env!("CARGO_MANIFEST_DIR"));
+
+    let mut file = File::open(filename).unwrap();
+
+    let mut db_fs_bytes = Vec::new();
+    file.read_to_end(&mut db_fs_bytes).unwrap();
+
+    let mut fs_data = FSAccounts::deserialize(&mut db_fs_bytes.as_slice()).unwrap();
+
+    let program_id = fs_data.owner_pubkey().unwrap();
+
+    let account_infos = fs_data.account_info_iter();
+    let fs = Rc::new(RefCell::new(
+        FS::from_account_iter(&program_id, &mut account_infos.iter()).unwrap(),
+    ));
+
+    let db = DB::from_segment(fs.clone(), DB_SEGMENT).unwrap();
+
+    let name_column = ColumnId::new(0);
+    let age_column = ColumnId::new(1);
+
+    let val = db.value(Data::Int(6), age_column).unwrap();
+
+    assert_eq!(val, None);
+
+    let val = db.value(Data::Int(0), age_column).unwrap();
+
+    assert_eq!(val, Some(Data::Int(22)));
+
+    let val = db.value(Data::Int(0), name_column).unwrap();
+
+    assert_eq!(val, Some(Data::ShortString("Alice".to_string())));
+
+    let val = db.value(Data::Int(6), name_column).unwrap();
+
+    assert_eq!(val, None);
+}
+
+#[test]
+fn value_secondary() {
+    let filename = format!("{}/tests/fs_images/prepared_db", env!("CARGO_MANIFEST_DIR"));
+
+    let mut file = File::open(filename).unwrap();
+
+    let mut db_fs_bytes = Vec::new();
+    file.read_to_end(&mut db_fs_bytes).unwrap();
+
+    let mut fs_data = FSAccounts::deserialize(&mut db_fs_bytes.as_slice()).unwrap();
+
+    let program_id = fs_data.owner_pubkey().unwrap();
+
+    let account_infos = fs_data.account_info_iter();
+    let fs = Rc::new(RefCell::new(
+        FS::from_account_iter(&program_id, &mut account_infos.iter()).unwrap(),
+    ));
+
+    let db = DB::from_segment(fs.clone(), DB_SEGMENT).unwrap();
+
+    let name_column = ColumnId::new(0);
+    let age_column = ColumnId::new(1);
+
+    let val = db
+        .value_secondary(
+            name_column,
+            Data::ShortString("Bob".to_string()),
+            age_column,
+        )
+        .unwrap();
+
+    assert_eq!(val, Some(Data::Int(23)));
+
+    let val = db
+        .value_secondary(
+            name_column,
+            Data::ShortString("Alex".to_string()),
+            age_column,
+        )
+        .unwrap();
+
+    assert_eq!(val, None);
+}
+
+#[test]
+fn delete_value() {
+    let filename = format!("{}/tests/fs_images/prepared_db", env!("CARGO_MANIFEST_DIR"));
+
+    let mut file = File::open(filename).unwrap();
+
+    let mut db_fs_bytes = Vec::new();
+    file.read_to_end(&mut db_fs_bytes).unwrap();
+
+    let mut fs_data = FSAccounts::deserialize(&mut db_fs_bytes.as_slice()).unwrap();
+
+    let program_id = fs_data.owner_pubkey().unwrap();
+
+    let account_infos = fs_data.account_info_iter();
+    let fs = Rc::new(RefCell::new(
+        FS::from_account_iter(&program_id, &mut account_infos.iter()).unwrap(),
+    ));
+
+    let mut db = DB::from_segment(fs.clone(), DB_SEGMENT).unwrap();
+
+    let name_column = ColumnId::new(0);
+    let age_column = ColumnId::new(1);
+
+    let val = db.delete_value(Data::Int(6), age_column).unwrap();
+
+    assert_eq!(val, false);
+
+    let val = db.delete_value(Data::Int(0), age_column).unwrap();
+
+    assert_eq!(val, true);
+
+    let val = db.delete_value(Data::Int(0), age_column).unwrap();
+
+    assert_eq!(val, false);
+}
+
+#[test]
+fn delete_value_secondary() {
+    let filename = format!("{}/tests/fs_images/prepared_db", env!("CARGO_MANIFEST_DIR"));
+
+    let mut file = File::open(filename).unwrap();
+
+    let mut db_fs_bytes = Vec::new();
+    file.read_to_end(&mut db_fs_bytes).unwrap();
+
+    let mut fs_data = FSAccounts::deserialize(&mut db_fs_bytes.as_slice()).unwrap();
+
+    let program_id = fs_data.owner_pubkey().unwrap();
+
+    let account_infos = fs_data.account_info_iter();
+    let fs = Rc::new(RefCell::new(
+        FS::from_account_iter(&program_id, &mut account_infos.iter()).unwrap(),
+    ));
+
+    let mut db = DB::from_segment(fs.clone(), DB_SEGMENT).unwrap();
+
+    let name_column = ColumnId::new(0);
+    let age_column = ColumnId::new(1);
+
+    let val = db
+        .delete_value_secondary(
+            name_column,
+            Data::ShortString("Bob".to_string()),
+            age_column,
+        )
+        .unwrap();
+
+    assert_eq!(val, true);
+
+    let val = db
+        .delete_value_secondary(
+            name_column,
+            Data::ShortString("Bob".to_string()),
+            age_column,
+        )
+        .unwrap();
+
+    assert_eq!(val, false);
 }
 
 // This function was used to create an image of empty FS, which is now used as a basis for DB
